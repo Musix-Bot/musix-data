47644161032974cae6b3a738cf1ca1e13fe38fe1
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2020-08-11 20:28:34 +0300

    urls and links

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import ids from "./data/ids.json";
 import colors from "./data/colors.json";
 import invites from "./data/invites.json";
+import urls from "./data/links.json";
 import redirects from "./data/redirs.json";
 export default {
   ids,
   colors,
   invites,
+  urls,
   redirects,
 };
