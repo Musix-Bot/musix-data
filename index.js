9aec4f4eda71aa5182e7c762cbf42564db20e59d
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2020-08-11 19:38:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ exports.ids = require("./data/ids.json");
 exports.colors = require("./data/colors.json");
 exports.redirects = require("./data/redirs.json");
 exports.invites = require("./data/invites.json");
+module.exports = {};
