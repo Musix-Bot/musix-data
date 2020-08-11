85924c6d3effef29a1fa089339f502d5ca263207
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2020-08-11 19:31:24 +0300

    Update index.d.ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,35 @@
+import { colors } from "..";
+
 declare module "musix-data" {
-  namespace data {}
+  interface colors {
+    607266889537945605: string;
+    727669741618987139: string;
+    649252971690983446: string;
+    607813867124555797: string;
+  }
+
+  interface ids {
+    testServer: string;
+    primary_test_channel: string;
+    secondary_test_channel: string;
+    devId: string;
+  }
+
+  interface invites {
+    releases: string;
+    supportServer: string;
+  }
+
+  interface redirs {
+    callback: string;
+  }
+
+  interface data {
+    colors: colors;
+    ids: ids;
+    invites: invites;
+    redirects: redirs;
+  }
 
   export = data;
 }
