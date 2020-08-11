fe86470447ca1cb5102f00a5534d1fb35198506b
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2020-08-11 19:19:44 +0300

    Init

---
+++ index.js
@@ -0,0 +1,4 @@
+exports.ids = require("./data/ids.json");
+exports.colors = require("./data/colors.json");
+exports.redirects = require("./data/redirs.json");
+exports.invites = require("./data/invites.json");
