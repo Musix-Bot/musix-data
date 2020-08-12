4615bfe944a7c1a1d9733a76da741b08921c5706
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2020-08-12 20:22:03 +0300

    Update index.d.ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,29 +1,28 @@
 declare module "musix-data" {
-  namespace data {
-    type colors = {
-      [index: string]: string;
-    };
-    type ids = {
-      testServer: string;
-      primary_test_channel: string;
-      secondary_test_channel: string;
-      devId: string;
-    };
-    type invites = {
-      releases: string;
-      supportServer: string;
-    };
-    type redirs = {
-      callback: string;
-    };
-  }
+  type colors = {
+    [index: string]: string;
+  };
+  type ids = {
+    testServer: string;
+    primary_test_channel: string;
+    secondary_test_channel: string;
+    devId: string;
+  };
+  type invites = {
+    releases: string;
+    supportServer: string;
+  };
+  type redirs = {
+    callback: string;
+  };
 
   type returnData = {
-    colors: data.colors;
-    ids: data.ids;
-    invites: data.invites;
-    redirects: data.redirs;
+    colors: colors;
+    ids: ids;
+    invites: invites;
+    redirects: redirs;
   };
   function data(): returnData;
+
   export = data;
 }
