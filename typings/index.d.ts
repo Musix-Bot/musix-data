e4481186dfcfcba844b429ba25ea77c100835662
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2020-08-12 20:02:02 +0300

    typings

---
+++ typings/index.d.ts
@@ -0,0 +1,25 @@
+declare module "musix-data" {
+  interface colors {
+    [index: string]: string;
+  }
+  interface ids {
+    testServer: string;
+    primary_test_channel: string;
+    secondary_test_channel: string;
+    devId: string;
+  }
+  interface invites {
+    releases: string;
+    supportServer: string;
+  }
+  interface redirs {
+    callback: string;
+  }
+  interface data {
+    colors: colors;
+    ids: ids;
+    invites: invites;
+    redirects: redirs;
+  }
+  export = data;
+}
