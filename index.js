895041d8e09e198e94e4b980d47a209facf878f0
MatteZ02 <matias.v.martikainen@gmail.com>
2020-11-02 15:15:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,15 @@ module.exports = {
     colors: colors,
     ids: ids,
     urls: urls,
+    isEqual: function (obj1, obj2) {
+        const propnames = Object.getOwnPropertyNames(obj1);
+
+        if (propnames.length != Object.getOwnPropertyNames(obj2).length) return false;
+
+        for (const propname of propnames) {
+            if (obj1[propname] !== obj2[propname]) return false;
+        }
+
+        return true;
+    }
 };
