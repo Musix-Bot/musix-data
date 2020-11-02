517bf79c7cb66514daeaec1fe27cace01c82035a
MatteZ02 <matias.v.martikainen@gmail.com>
2020-11-02 14:09:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const ids = require("./data/ids.json");
 const urls = require("./data/links.json");
 
 
-module.exports = function () {
-    return {
-        colors: colors,
-        ids: ids,
-        urls: urls,
-    }
+module.exports = {
+    colors: colors,
+    ids: ids,
+    urls: urls,
 };
